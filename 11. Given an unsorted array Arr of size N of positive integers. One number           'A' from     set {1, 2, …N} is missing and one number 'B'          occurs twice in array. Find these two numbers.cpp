764b831a86b6b5faf3dfc0d0a0947b6252ee37be
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-04 13:29:35 +0530

    Create 11. Given an unsorted array Arr of size N of positive integers. One number           'A' from     set {1, 2, …N} is missing and one number 'B'          occurs twice in array. Find these two numbers.cpp

---
+++ 11. Given an unsorted array Arr of size N of positive integers. One number           'A' from     set {1, 2, …N} is missing and one number 'B'          occurs twice in array. Find these two numbers.cpp
@@ -0,0 +1,22 @@
+class Solution{
+public:
+    vector<int> findTwoElement(int arr[], int n) {
+        int one=0, xr =0, two=0, f=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[abs(arr[i])-1]<0 && f==0) {one = abs(arr[i]); f=1;}
+            else arr[abs(arr[i])-1] = -arr[abs(arr[i])-1];
+            xr ^= i+1;
+            xr ^= abs(arr[i]);
+        }
+        xr^=one;
+        
+        two = xr;
+        //cout<<one<<" "<<two;
+        vector<int> v(2) ;
+        v[1] = two;
+        v[0] = one;
+        
+        return v;
+    }
+};
