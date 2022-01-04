4a0dd41e17e0f41a5ad3ab0b4b3e33cf99a538b6
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-04 15:17:27 +0530

    Update and rename 12. Find total number of Squares in a N*N chessboard to 12. Find total number of Squares in a N*N chessboard.cpp

--- 12. Find total number of Squares in a N*N chessboard.cpp
+++ 12. Find total number of Squares in a N*N chessboard.cpp
@@ -1,3 +1,11 @@
 total no. of sqaures = N*N(square size 1) + (N-1)*(N-1)(square size 2) + (N-2)*(N-2)(square size 3) + . . . 3*3(square size N-3) + 2*2(square size N-1) + 1*1(square size N) ;
                      = summation(n^2) from 1 to n;
                      = n*(n+1)*(2n+1)/6;
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        // code here
+        
+        return N*(N+1)*(2*N+1)/6;
+    }
+};
