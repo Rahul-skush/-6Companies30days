b4e65d70f8663a11bfba14644d91617db24c2b47
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-05 10:43:42 +0530

    Create 5. Program to find Nth Ugly Number.cpp

---
+++ 5. Program to find Nth Ugly Number.cpp
@@ -0,0 +1,21 @@
+class Solution{
+public:	
+	// #define ull unsigned long long
+	/* Function to get the nth ugly number*/
+	ull getNthUglyNo(int n) {
+	    // code here
+	    
+	    vector<ull> v(n);
+	    int t1 =0, t2 =0, t3 =0;
+	    v[0] =1;
+	    for(int i=1;i<n;i++)
+	    {
+	        v[i] = min({v[t1]*2, v[t2]*3, v[t3]*5});
+	        if(v[i]==v[t1]*2) t1++;
+	        if(v[i]==v[t2]*3) t2++;
+	        if(v[i]==v[t3]*5) t3++;
+	        //cout<<v[i]<<" ";
+	    }
+	   return v[n-1];
+	}
+};
