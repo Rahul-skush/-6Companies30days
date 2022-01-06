9c3c8dfd02c6ad770fcc77b101e5b38b4d04dcb0
Rahul Singh <43444047+Rahul-skush@users.noreply.github.com>
2022-01-06 15:55:58 +0530

    Create 9. Which among them forms a perfect Sudoku Pattern ?.cpp

---
+++ Amazon/9. Which among them forms a perfect Sudoku Pattern ?.cpp
@@ -0,0 +1,35 @@
+class Solution{
+public:
+    int isValid(vector<vector<int>> mat){
+        // code here
+        int n = 9;
+        for(int i=0;i<n;i++)
+        {
+            vector<int> v1(9, 0), v2(9, 0);
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]!=0)
+                {v1[mat[i][j]-1]++; 
+                if(v1[mat[i][j]-1]>1) return 0;}
+                if(mat[j][i]!=0)
+                {v2[mat[j][i]-1]++;
+                if(v2[mat[j][i]-1]>1) return 0;}
+                
+                if(i%3==0 && j%3==0)
+                {
+                    vector<int> v3(9, 0);
+                    for(int k=i;k<i+3;k++)
+                    {
+                        for(int l=j;l<j+3;l++)
+                        {
+                            if(mat[k][l]!=0)
+                            {v3[mat[k][l]-1]++;
+                            if(v3[mat[k][l]-1]>1) return 0;}
+                        }
+                    }
+                }
+            }
+        }
+        return 1;
+    }
+};
